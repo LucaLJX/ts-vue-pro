5129d1d4bd016d30a8843233da82df5525e4c8a1
luca_ljx <liujingxin06127@hellobike.com>
2018-10-31 22:53:40 +0800

    U - 引入element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './App'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI, { size: 'mini' })
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  components: { App },
   render: (h) => h(App)
 }).$mount('#app')
