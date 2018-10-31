2e7b16e19b17869af101841172df0f58a0a520e6
luca_ljx <liujingxin06127@hellobike.com>
2018-10-31 22:58:20 +0800

    U - 定义router.ts格式

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/Home/index'
 
 Vue.use(Router)
 
@@ -10,8 +10,40 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'Welcome',
+      component: Home,
+      redirect: '/home',
+      children: []
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      meta: { // 附加数据
+        desc: '首页', // 描述 - 用于展示菜单名称
+        authority: '',
+        icon: 'el-icon-info'
+      },
+      children: [
+        {
+          path: '123',
+          component: Home,
+          meta: { // 附加数据
+            desc: '首页', // 描述 - 用于展示菜单名称
+            authority: '',
+            icon: ''
+          }
+        },
+        {
+          path: '4123',
+          component: Home,
+          meta: { // 附加数据
+            desc: '首页', // 描述 - 用于展示菜单名称
+            authority: '', // 权限
+            icon: '' // 图标
+          }
+        }
+      ]
     },
     {
       path: '/about',
@@ -19,7 +51,13 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      meta: {
+        desc: '关于我',
+        authority: '',
+        icon: ''
+      },
+      children: []
     }
   ]
 })
